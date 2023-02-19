8479e1e147c83f01112be61f350232dfa5a84ece
Tom Smith <thomas-smith@sky.com>
2023-02-19 14:29:44 +0000

    fixed bug in build

--- blockware/main.go
+++ blockware/main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
+//go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
@@ -50,8 +51,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
